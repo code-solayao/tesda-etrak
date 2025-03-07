0fcc49e821433909cc6817754fd0ed5313489cad
Dave Andrew Solayao <solayao.daveandrew@gmail.com>
2025-03-07 16:07:22 +0800

    Update view need more fix
    - Particularly when not clicking the Verification tab which results to Referral Status and Referral Date losing values
    - Completed the fundamental functionality

--- tesda_etrak/app/Http/Controllers/EtrakController.php
+++ tesda_etrak/app/Http/Controllers/EtrakController.php
@@ -44,11 +44,12 @@ class EtrakController extends Controller
             'allocation' => ['nullable', 'string', 'max:50']
         ]);
 
-        $last_name = strip_tags($validated['last_name']);
-        $first_name = strip_tags($validated['first_name']);
-        $middle_name = strip_tags($validated['middle_name']);
-        $extension_name = strip_tags($validated['extension_name']);
-        $full_name = $this->full_name_format($last_name, $first_name, $middle_name, $extension_name);
+        foreach ($validated as $key => $value) {
+            $validated[$key] = strip_tags($value);
+        }
+
+        $full_name = $this->full_name_format($validated['last_name'], $validated['first_name'], $validated['middle_name'], $validated['extension_name']);
+        $sex = isset($validated['sex']) == true ? $validated['sex'] : '';
 
         Graduate::create([
             'district' => $validated['district'], 
@@ -61,7 +62,7 @@ class EtrakController extends Controller
             'middle_name' => $validated['middle_name'], 
             'extension_name' => $validated['extension_name'], 
             'full_name' => $full_name, 
-            'sex' => isset($validated['sex']) == true ? $validated['sex'] : '', 
+            'sex' => $sex, 
             'birthdate' => $validated['birthdate'], 
             'contact_number' => $validated['contact_number'], 
             'email' => $validated['email'], 
@@ -84,22 +85,6 @@ class EtrakController extends Controller
 
     public function update_record(Graduate $graduate, Request $request) {
         $validated = $request->validate([
-            'district' => ['nullable', 'string', 'max:50'], 
-            'city' => ['nullable', 'string', 'max:50'], 
-            'tvi' => ['nullable', 'string', 'max:255'], 
-            'qualification_title' => ['nullable', 'string', 'max:255'], 
-            'sector' => ['nullable', 'string', 'max:255'], 
-            'last_name' => ['required', 'string', 'max:255'], 
-            'first_name' => ['required', 'string', 'max:255'], 
-            'middle_name' => ['nullable', 'string', 'max:255'], 
-            'extension_name' => ['nullable', 'string', 'max:50'], 
-            'sex' => ['nullable', 'string', 'max:50'], 
-            'birthdate' => ['nullable', 'string', 'max:50'], 
-            'contact_number' => ['nullable', 'string', 'min:13', 'max:16'], 
-            'email' => ['nullable', 'email', 'max:255'], 
-            'scholarship_type' => ['nullable', 'string', 'max:50'], 
-            'address' => ['nullable', 'string', 'max:255'], 
-            'allocation' => ['nullable', 'string', 'max:50'], 
             'verification_means' => ['nullable', 'string', 'max:50'], 
             'verification_date' => ['nullable', 'string', 'max:50'], 
             'verification_status' => ['nullable', 'string', 'max:50'], 
@@ -121,46 +106,50 @@ class EtrakController extends Controller
             'not_hired_reason' => ['nullable', 'string', 'max:50']
         ]);
 
+        foreach ($validated as $key => $value) {
+            $validated[$key] = strip_tags($value);
+        }
+
+        $verification_status = isset($validated['verification_status']) == true ? $validated['verification_status'] : '';
+        $response_status = isset($validated['response_status']) == true ? $validated['response_status'] : '';
+        $not_interested_reason = isset($validated['not_interested_reason']) == true ? $validated['not_interested_reason'] : '';
+        $referral_status = isset($validated['referral_status']) == true ? $validated['referral_status'] : '';
+        $referral_date = isset($validated['referral_date']) == true ? $validated['referral_date'] : '';
+        $no_referral_reason = isset($validated['no_referral_reason']) == true ? $validated['no_referral_reason'] : '';
+        $invalid_contact = isset($validated['invalid_contact']) == true ? $validated['invalid_contact'] : '';
+
+        $company_name = isset($validated['company_name']) == true ? $validated['company_name'] : '';
+        $company_address = isset($validated['company_address']) == true ? $validated['company_address'] : '';
+        $job_title = isset($validated['job_title']) == true ? $validated['job_title'] : '';
+        $employment_status = isset($validated['employment_status']) == true ? $validated['employment_status'] : '';
+        $hired_date = isset($validated['hired_date']) == true ? $validated['hired_date'] : '';
+        $submission_documents_date = isset($validated['submission_documents_date']) == true ? $validated['submission_documents_date'] : '';
+        $interview_date = isset($validated['interview_date']) == true ? $validated['interview_date'] : '';
+        $not_hired_reason = isset($validated['not_hired_reason']) == true ? $validated['not_hired_reason'] : '';
+
         $graduate->update([
-            'district' => $validated['district'], 
-            'city' => $validated['city'], 
-            'tvi' => $validated['tvi'], 
-            'qualification_title' => $validated['qualification_title'], 
-            'sector' => $validated['sector'], 
-            'last_name' => $validated['last_name'], 
-            'first_name' => $validated['first_name'], 
-            'middle_name' => $validated['middle_name'], 
-            'extension_name' => $validated['extension_name'], 
-            'full_name' => $validated['full_name'], 
-            'sex' => isset($validated['sex']) == true ? $validated['sex'] : '', 
-            'birthdate' => $validated['birthdate'], 
-            'contact_number' => $validated['contact_number'], 
-            'email' => $validated['email'], 
-            'scholarship_type' => $validated['scholarship_type'], 
-            'address' => $validated['address'], 
-            'allocation' => $validated['allocation'], 
-            'verification_means' => ['nullable', 'string', 'max:50'], 
-            'verification_date' => ['nullable', 'string', 'max:50'], 
-            'verification_status' => isset($validated['verification_status']) == true ? $validated['verification_status'] : '', 
-            'follow_up_date_1' => ['nullable', 'string', 'max:50'], 
-            'follow_up_date_2' => ['nullable', 'string', 'max:50'], 
-            'response_status' => isset($validated['response_status']) == true ? $validated['response_status'] : '', 
-            'not_interested_reason' => ['nullable', 'string', 'max:255'], 
-            'referral_status' => isset($validated['referral_status']) == true ? $validated['referral_status'] : '', 
-            'referral_date' => ['nullable', 'string', 'max:50'], 
-            'no_referral_reason' => ['nullable', 'string', 'max:255'], 
-            'invalid_contact' => isset($validated['invalid_contact']) == true ? $validated['invalid_contact'] : '', 
-            'company_name' => ['nullable', 'string', 'max:255'], 
-            'company_address' => ['nullable', 'string', 'max:255'], 
-            'job_title' => ['nullable', 'string', 'max:255'], 
-            'employment_status' => isset($validated['employment_status']) == true ? $validated['employment_status'] : '', 
-            'hired_date' => ['nullable', 'string', 'max:50'], 
-            'submitted_documents_date' => ['nullable', 'string', 'max:50'], 
-            'interview_date' => ['nullable', 'string', 'max:50'], 
-            'not_hired_reason' => ['nullable', 'string', 'max:50']
+            'verification_means' => $validated['verification_means'], 
+            'verification_date' => $validated['verification_date'], 
+            'verification_status' => $verification_status, 
+            'follow_up_date_1' => $validated['follow_up_date_1'], 
+            'follow_up_date_2' => $validated['follow_up_date_2'], 
+            'response_status' => $response_status, 
+            'not_interested_reason' => $not_interested_reason, 
+            'referral_status' => $referral_status, 
+            'referral_date' => $referral_date, 
+            'no_referral_reason' => $no_referral_reason, 
+            'invalid_contact' => $invalid_contact, 
+            'company_name' => $company_name, 
+            'company_address' => $company_address, 
+            'job_title' => $job_title, 
+            'employment_status' => $employment_status, 
+            'hired_date' => $hired_date, 
+            'submitted_documents_date' => $submission_documents_date, 
+            'interview_date' => $interview_date, 
+            'not_hired_reason' => $not_hired_reason
         ]);
 
-        return redirect()->route('record_details', $graduate->id)->with('success', 'Updated record successfully!');
+        return redirect()->route('record-details', $graduate->id)->with('success', 'Updated record successfully!');
     }
 
     private function full_name_format($last_name, $first_name, $middle_name, $extension_name) {
--- tesda_etrak/resources/views/e-trak/update-record.blade.php
+++ tesda_etrak/resources/views/e-trak/update-record.blade.php
@@ -111,7 +111,7 @@
                     <select name="verification_means" id="verification_means" class="form-control">
                         <option value="">-- Select a means of verification --</option>
                         @foreach ($verification_means as $means)
-                            <option value="{{ $means }}" {{ old('verification_means') == $means ? 'selected' : '' }}>
+                            <option value="{{ $means }}" {{ old('verification_means', $graduate->verification_means) == $means ? 'selected' : '' }}>
                                 {{ $means }}
                             </option>
                         @endforeach
@@ -119,16 +119,16 @@
                 </div>
                 <div class="form-group mb-4">
                     <label for="verification_date" class="form-label control-label-1">Date of Verification</label>
-                    <input type="date" class="form-control" id="verification_date" name="verification_date" value="{{ old('verification_date') }}" />
+                    <input type="date" class="form-control" id="verification_date" name="verification_date" value="{{ old('verification_date', $graduate->verification_date) }}" />
                 </div>
                 <div class="form-group">
                     <label for="respondedBtn" class="form-label control-label-1">Status of Verification</label>
 
                     <div class="form-check">
-                        <input type="radio" class="form-check-input" id="respondedBtn" name="verification_status" value="Responded" {{ old('verification_status') == "Responded" ? 'checked' : '' }} />
+                        <input type="radio" class="form-check-input" id="respondedBtn" name="verification_status" value="Responded" {{ old('verification_status', $graduate->verification_status) == "Responded" ? 'checked' : '' }} />
                         <label class="form-label" for="respondedBtn">Responded</label>
                         <br>
-                        <input type="radio" class="form-check-input" id="noResponseBtn" name="verification_status" value="No Response" {{ old('verification_status') == "No Response" ? 'checked' : '' }} />
+                        <input type="radio" class="form-check-input" id="noResponseBtn" name="verification_status" value="No Response" {{ old('verification_status', $graduate->verification_status) == "No Response" ? 'checked' : '' }} />
                         <label class="form-label" for="noResponseBtn">No Response</label>
                     </div>
                 </div>
@@ -137,32 +137,32 @@
                         <label class="form-label control-label-1">Type of Response</label>
 
                         <div style="margin-left: 30px" class="form-check">
-                            <input type="radio" class="form-check-input" id="interestedBtn" name="response_status" value="Interested" {{ old('response_status') == "Interested" ? 'checked' : '' }} />
+                            <input type="radio" class="form-check-input" id="interestedBtn" name="response_status" value="Interested" {{ old('response_status', $graduate->response_status) == "Interested" ? 'checked' : '' }} />
                             <label for="interestedBtn" class="form-label">Interested</label>
                             <div>
                                 <label class="form-label control-label-2">Refer to Company?</label>
 
                                 <div style="margin-left: 30px">
                                     <fieldset id="referralStatusForm" disabled>
-                                        <input type="radio" class="form-check-input" id="referYesBtn" name="referral_status" value="Yes" {{ old('referral_status') == "Yes" ? 'checked' : '' }} />
+                                        <input type="radio" class="form-check-input" id="referYesBtn" name="referral_status" value="Yes" {{ old('referral_status', $graduate->referral_status) == "Yes" ? 'checked' : '' }} />
                                         <label for="referYesBtn" class="form-label">YES</label>
                                         <br>
-                                        <input type="date" class="form-control" id="referralDate" name="referral_date" value="{{ old('referral_date') }}" disabled />
+                                        <input type="date" class="form-control" id="referralDate" name="referral_date" value="{{ old('referral_date', $graduate->referral_date) }}" disabled />
                                         <br>
-                                        <input type="radio" class="form-check-input" id="referNoBtn" name="referral_status" value="No" {{ old('referral_status') == "No" ? 'checked' : '' }} />
+                                        <input type="radio" class="form-check-input" id="referNoBtn" name="referral_status" value="No" {{ old('referral_status', $graduate->referral_status) == "No" ? 'checked' : '' }} />
                                         <label for="referNoBtn" class="form-label">NO</label>
                                         <br>
                                         <label for="noReferralReason" class="form-label">Reason</label>
-                                        <input type="text" class="form-control" id="noReferralReason" name="no_referral_reason" value="{{ old('no_referral_reason') }}" disabled />
+                                        <input type="text" class="form-control" id="noReferralReason" name="no_referral_reason" value="{{ old('no_referral_reason', $graduate->no_referral_reason) }}" disabled />
                                     </fieldset>
                                 </div>
                             </div><br>
 
-                            <input type="radio" class="form-check-input" id="notInterestedBtn" name="response_status" value="Not Interested" {{ old('response_status') == "Not Interested" ? 'checked' : '' }} />
+                            <input type="radio" class="form-check-input" id="notInterestedBtn" name="response_status" value="Not Interested" {{ old('response_status', $graduate->response_status) == "Not Interested" ? 'checked' : '' }} />
                             <label for="notInterestedBtn" class="form-label">Not Interested</label>
                             <div>
                                 <label for="notInterestedReason" class="form-label">Reason</label>
-                                <textarea name="not_interested_reason" id="notInterestedReason" class="form-control" rows="2" disabled>{{ old('not_interested_reason') }}</textarea>
+                                <textarea name="not_interested_reason" id="notInterestedReason" class="form-control" rows="2" disabled>{{ old('not_interested_reason', $graduate->not_interested_reason) }}</textarea>
                             </div>
                         </div>
                     </div>
@@ -173,14 +173,14 @@
 
                         <div style="margin-left: 30px" class="mb-4">
                             <label for="followup1" class="form-label">First Follow-up</label>
-                            <input type="date" class="form-control" id="followup1" name="follow_up_date_1" value="{{ old('follow_up_date_1') }}" />
+                            <input type="date" class="form-control" id="followup1" name="follow_up_date_1" value="{{ old('follow_up_date_1', $graduate->follow_up_date_1) }}" />
                         </div>
                         <div style="margin-left: 30px" class="mb-4">
                             <label for="followup2" class="form-label">Second Follow-up</label>
-                            <input type="date" class="form-control" id="followup2" name="follow_up_date_2" value="{{ old('follow_up_date_2') }}" />
+                            <input type="date" class="form-control" id="followup2" name="follow_up_date_2" value="{{ old('follow_up_date_2', $graduate->follow_up_date_2) }}" />
                         </div>
                         <div style="margin-left: 30px" class="form-check mb-4">
-                            <input type="checkbox" class="form-check-input" id="invalidContact" name="invalid_contact" value="Yes" {{ old('invalid_contact') == 'Yes' ? 'checked' : '' }} />
+                            <input type="checkbox" class="form-check-input" id="invalidContact" name="invalid_contact" value="Yes" {{ old('invalid_contact', $graduate->invalid_contact) == 'Yes' ? 'checked' : '' }} />
                             <label for="invalidContact" class="form-check-label">Invalid Contact</label>
                         </div>
                     </div>
@@ -196,49 +196,43 @@
                 <fieldset id="employmentField" disabled>
                     <div class="form-group mb-4">
                         <label for="companyName" class="form-label control-label-1">Name of Company</label>
-                        <input type="text" class="form-control" id="companyName" name="company_name" value="{{ old('company_name') }}" />
+                        <input type="text" class="form-control" id="companyName" name="company_name" value="{{ old('company_name', $graduate->company_name) }}" />
                     </div>
                     <div class="form-group mb-4">
                         <label for="companyAddress" class="form-label control-label-1">Address</label>
-                        <input type="text" class="form-control" id="companyAddress" row="5" name="company_address" value="{{ old('company_address') }}" />
+                        <input type="text" class="form-control" id="companyAddress" row="5" name="company_address" value="{{ old('company_address', $graduate->company_address) }}" />
                     </div>
                     <div class="form-group mb-4">
                         <label for="jobTitle" class="form-label control-label-1">Job Title</label>
-                        <input type="text" class="form-control" id="jobTitle" name="job_title" value="{{ old('job_title') }}" />
+                        <input type="text" class="form-control" id="jobTitle" name="job_title" value="{{ old('job_title', $graduate->job_title) }}" />
                     </div>
                     <div class="form-group mb-4">
                         <label class="form-label control-label-1">Employment Status</label>
 
                         <div class="form-check">
-                            <?php
-                                $hired = "Hired";
-                                $submit_docs = "Submitted Documents";
-                                $for_interview = "For Interview";
-                                $not_hired = "Not Hired";
-                            ?>
-                            <input type="radio" class="form-check-input" id="hired" name="employment_status" value="Hired" {{ old('employment_status') == 'Hired' ? 'checked' : '' }} />
+                            <input type="radio" class="form-check-input" id="hired" name="employment_status" value="Hired" {{ old('employment_status', $graduate->employment_status) == 'Hired' ? 'checked' : '' }} />
                             <label for="hired" class="form-label employment-status-label">Hired</label>
                             <div class="indent mb-4">
-                                <input type="date" class="form-control" id="hiredDate" name="hired_date" value="{{ old('hired_date') }}" disabled />
+                                <input type="date" class="form-control" id="hiredDate" name="hired_date" value="{{ old('hired_date', $graduate->hired_date) }}" disabled />
                             </div>
-                            <input type="radio" class="form-check-input" id="submitDocs" name="employment_status" value="Submitted Documents" {{ old('employment_status') == 'Submitted Documents' ? 'checked' : '' }} />
+                            <input type="radio" class="form-check-input" id="submitDocs" name="employment_status" value="Submitted Documents" {{ old('employment_status', $graduate->employment_status) == 'Submitted Documents' ? 'checked' : '' }} />
                             <label for="submitDocs" class="form-label employment-status-label">Submitted Documents</label>
                             <div class="indent-1 mb-4">
-                                <input type="date" class="form-control" id="submitDocsDate" name="submitted_documents_date" value="{{ old('submitted_documents_date') }}" disabled />
+                                <input type="date" class="form-control" id="submitDocsDate" name="submitted_documents_date" value="{{ old('submitted_documents_date', $graduate->submitted_documents_date) }}" disabled />
                             </div>
-                            <input type="radio" class="form-check-input" id="forInterview" name="employment_status" value="For Interview" {{ old('employment_status') == 'For Interview' ? 'checked' : '' }} />
+                            <input type="radio" class="form-check-input" id="forInterview" name="employment_status" value="For Interview" {{ old('employment_status', $graduate->employment_status) == 'For Interview' ? 'checked' : '' }} />
                             <label for="forInterview" class="form-label employment-status-label">For Interview</label>
                             <div class="indent-1 mb-4">
-                                <input type="date" class="form-control" id="interviewDate" name="interview_date" value="{{ old('interview_date') }}" disabled />
+                                <input type="date" class="form-control" id="interviewDate" name="interview_date" value="{{ old('interview_date', $graduate->interview_date) }}" disabled />
                             </div>
-                            <input type="radio" class="form-check-input" id="notHired" name="employment_status" value="Not Hired" {{ old('employment_status') == 'Not Hired' ? 'checked' : '' }} />
+                            <input type="radio" class="form-check-input" id="notHired" name="employment_status" value="Not Hired" {{ old('employment_status', $graduate->employment_status) == 'Not Hired' ? 'checked' : '' }} />
                             <label for="notHired" class="form-label employment-status-label">Not Hired</label>
                             <div class="indent-1 mb-4">
                                 <label for="notHiredReason" class="form-label">Reason</label>
                                 <select name="not_hired_reason" id="notHiredReason" class="form-control" disabled>
                                     <option value="">-- Select a reason --</option>
                                     @foreach ($not_hired_reasons as $reason)
-                                        <option value="{{ $reason }}" {{ old('not_hired_reason') == $reason ? 'selected' : '' }}>
+                                        <option value="{{ $reason }}" {{ old('not_hired_reason', $graduate->not_hired_reason) == $reason ? 'selected' : '' }}>
                                             {{ $reason }}
                                         </option>
                                     @endforeach
