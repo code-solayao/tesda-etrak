654f4c156c2149e6f7a06c2ac25d8c217c6e289d
Dave Andrew Solayao <solayao.daveandrew@gmail.com>
2025-05-07 16:29:51 +0800

    Export function successful
    - Function for MySQL database export worked successfully
    - Need to test and debug
    - Don't forget to change spreadsheet ID and sheet name

--- tesda_etrak/app/Console/Commands/ExportMySqlData.php
+++ tesda_etrak/app/Console/Commands/ExportMySqlData.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use App\Http\Controllers\GoogleSheetsService;
 use App\Models\Graduate;
+use Google\Client;
+use Google\Service\Sheets;
 use Illuminate\Console\Command;
 
 class ExportMySqlData extends Command
@@ -30,30 +32,80 @@ class ExportMySqlData extends Command
     {
         $this->info('MySQL database export starts...');
 
-        $spreadsheetId = env('GOOGLE_SHEET_ID');
-        $range = 'Exported Data';
+        $client = new Client();
+        $client->setAuthConfig(storage_path('app/credentials.json'));
+        $client->addScope(Sheets::SPREADSHEETS);
+        $service = new Sheets($client);
 
-        Graduate::chunk(1000, function($rows) use ($spreadsheetId, $range) {
+        $spreadsheetId = env('TEST_SHEET_ID');
+        $range = 'Sheet1';
+
+        Graduate::chunk(1000, function ($rows) use ($service, $spreadsheetId, $range) {
             $values = [];
 
             foreach ($rows as $row) {
                 $values[] = [
-                    $row->column1, 
-                    // dagdagan pa syempre
+                    $row->district, 
+                    $row->city, 
+                    $row->tvi, 
+                    $row->qualification_title, 
+                    $row->sector, 
+                    $row->last_name, 
+                    $row->first_name, 
+                    $row->middle_name, 
+                    $row->extension_name, 
+                    $row->full_name, 
+                    $row->sex, 
+                    $row->birthdate, 
+                    $row->contact_number, 
+                    $row->email, 
+                    $row->address, 
+                    $row->scholarship_type, 
+                    $row->training_status, 
+                    $row->assessment_result, 
+                    $row->employment_before_training, 
+                    $row->occupation, 
+                    $row->employer_name, 
+                    $row->employment_type, 
+                    $row->work_address, 
+                    $row->date_hired, 
+                    $row->allocation, 
+                    $row->verification_means, 
+                    $row->verification_date, 
+                    $row->verification_status, 
+                    $row->follow_up_date_1, 
+                    $row->response_status, 
+                    $row->not_interested_reason, 
+                    $row->referral_status, 
+                    $row->company_name, 
+                    $row->company_address, 
+                    $row->job_title, 
+                    $row->employment_status, 
+                    $row->hired_date, 
+                    $row->not_hired_reason, 
+                    $row->count, 
+                    $row->no_of_graduates, 
+                    $row->no_of_employed, 
+                    $row->verification, 
+                    $row->job_vacancies, 
+                    $row->remarks, 
+                    $row->application_status
                 ];
             }
 
-            // kulang pa
+
+            $body = new Sheets\ValueRange([
+                'values' => $values
+            ]);
+
+            $params = ['valueInputOption' => 'RAW'];
+
+            $service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
+
             $this->info(count($values) . ' rows appended. ');
         });
 
         $this->info('MySQL database export complete.');
         return Command::SUCCESS;
-    }
-
-    public function __construct(GoogleSheetsService $sheetsService)
-    {
-        parent::__construct();
-        $this->sheetsService = $sheetsService;
     }
 }
