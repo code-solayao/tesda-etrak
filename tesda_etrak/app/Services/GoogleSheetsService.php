b9c0554a4142bcaa42bb9dce9d41c89d77d0638b
Dave Andrew Solayao <solayao.daveandrew@gmail.com>
2025-07-04 10:52:15 +0800

    Added GoogleSheetsService class

---
+++ tesda_etrak/app/Services/GoogleSheetsService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use Google\Client;
+use Google\Service\Sheets;
+
+class GoogleSheetsService
+{
+    protected $client;
+    protected $service;
+    protected $spreadsheetId;
+
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        $this->spreadsheetId = env('EMPLOYMENT_MONITORING_SYSTEM_ID');
+
+        $this->client = new Client();
+        $this->client->setAuthConfig(storage_path('app/credentials.json'));
+        $this->client->addScope(Sheets::SPREADSHEETS);
+
+        $this->service = new Sheets($this->client);
+    }
+
+    public function updateRows($range, array $values) 
+    {
+        
+    }
+
+    public function appendRows($range, array $values) 
+    {
+        $body = new Sheets\ValueRange([
+            'values' => $values
+        ]);
+
+        $params = ['valueInputOption' => 'RAW'];
+
+        return $this->service->spreadsheets_values->append($this->spreadsheetId, $range, $body, $params);
+    }
+}
