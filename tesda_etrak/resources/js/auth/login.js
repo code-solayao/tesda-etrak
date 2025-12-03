70149eb4edbed4bda76500cefedf545ec29d28d9
Dave Andrew Solayao <solayao.daveandrew@gmail.com>
2025-12-03 17:29:16 +0800

    Login mobile view in progress
    - Working on the Login mobile view in progress
    - Need to adjust design on the login form for mobile view

---
+++ tesda_etrak/resources/js/auth/login.js
@@ -0,0 +1,30 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let bodyRows = document.querySelectorAll(".body-row");
+//     let form = document.getElementById("deleteForm");
+
+//     bodyRows.forEach(function (row) {
+//         row.addEventListener('dblclick', function () {
+//             window.location = row.dataset.url;
+//         });
+//     });
+
+//     document.querySelectorAll(".action-select").forEach(select => {
+//         select.addEventListener("change", function (event) {
+//             event.stopPropagation();
+//             const id = select.dataset.id;
+//             const value = select.value;
+//             const option = select.options[select.selectedIndex];
+
+//             if (value === "view" || value === "update") {
+//                 window.location = option.dataset.url;
+//             }
+
+//             if (value === "delete") {
+//                form.action = `/admin/list-of-graduates/record-details/${id}`; 
+//                document.getElementById("deleteModal").classList.remove('hidden');
+//             }
+
+//             select.value = "";
+//         });
+//     });
+// });
